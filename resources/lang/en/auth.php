a40e0330c5efc751e0354565cbb5e10b61e63cac
Javier Nunez Morales <javier.nunez.morales.98@gmail.com>
2021-12-10 13:39:49 +0100

    Prac. 10/12 - Mensajes de error de login actualizados

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Los datos introducidos no coinciden',
+    'password' => 'La contraseña introducida es incorrecta',
+    'throttle' => 'Muchos intentos de acceso, por favor intentelo de nuevo dentro de :seconds seconds.',
 
 ];
