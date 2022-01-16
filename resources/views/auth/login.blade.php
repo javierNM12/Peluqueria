f33747eb07d0a7d75401c70bb94fa9379cd03213
javierNM12 <javier.nunez.morales.98@gmail.com>
2022-01-16 16:49:02 +0100

    Build lista para heroku

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
             <div>
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus placeholder="yadira@peluqueria.com" />
             </div>
 
             <!-- Password -->
@@ -31,7 +31,7 @@
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required autocomplete="current-password" />
+                                required autocomplete="current-password" placeholder="yadira" />
             </div>
 
             <!-- Remember Me -->
